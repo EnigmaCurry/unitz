6e23f05ecbb2312d2a9cf34308e7b88e486ccc65
EnigmaCurry <ryan@enigmacurry.com>
2026-05-28 21:33:01 +0000

    stuff

--- src/unitz/cli.clj
+++ src/unitz/cli.clj
@@ -5,6 +5,101 @@
             [unitz.parser :as parser])
   (:gen-class))
 
+(def dim-labels
+  {{:length 1} "Length"
+   {:length 2} "Area"
+   {:length 3} "Volume"
+   {:mass 1} "Mass"
+   {:time 1} "Time"
+   {:data 1} "Data"
+   {:mass 1 :length 1 :time -2} "Force"
+   {:mass 1 :length 2 :time -2} "Energy"
+   {:mass 1 :length 2 :time -3} "Power"
+   {:mass 1 :length -1 :time -2} "Pressure"})
+
+(defn reverse-aliases
+  "Build a map from canonical unit keyword to sorted list of alias strings."
+  []
+  (reduce-kv
+   (fn [m alias-str unit-kw]
+     (update m unit-kw (fnil conj []) alias-str))
+   {}
+   parser/unit-aliases))
+
+(def label->dim
+  (into {} (map (fn [[k v]] [(str/lower-case v) k]) dim-labels)))
+
+(def all-kind-names
+  (sort (concat (map (comp str/lower-case val) dim-labels)
+                ["temperature" "compound"])))
+
+(defn format-section [label entries aliases]
+  (let [lines (for [[unit-kw _] (sort-by (comp str first) entries)
+                    :let [names (sort (get aliases unit-kw))]]
+                (str "  " (name unit-kw)
+                     (when (seq names)
+                       (str "  (" (str/join ", " names) ")"))))]
+    (str label ":\n" (str/join "\n" lines))))
+
+(defn format-temp-section [aliases]
+  (let [temp-units [{:key :degF :aliases (get aliases :degF)}
+                    {:key :degC :aliases (get aliases :degC)}
+                    {:key :K    :aliases (get aliases :K)}]]
+    (str "Temperature:\n"
+         (str/join "\n"
+                   (for [{:keys [key aliases]} temp-units
+                         :let [names (sort aliases)]]
+                     (str "  " (name key)
+                          (when (seq names)
+                            (str "  (" (str/join ", " names) ")"))))))))
+
+(defn format-compound-section []
+  (str "Compound:\n"
+       (str/join "\n"
+                 (for [[alias-str _] (sort parser/special-unit-forms)]
+                   (str "  " alias-str)))))
+
+(defn list-units
+  ([] (list-units nil))
+  ([kind]
+   (let [aliases (reverse-aliases)
+         by-dim (group-by (fn [[_ v]] (:dim v)) u/unit-defs)
+         dim-order [{:length 1} {:length 2} {:length 3}
+                    {:mass 1} {:time 1} {:data 1}
+                    {:mass 1 :length 1 :time -2}
+                    {:mass 1 :length 2 :time -2}
+                    {:mass 1 :length 2 :time -3}
+                    {:mass 1 :length -1 :time -2}]
+         kind-lower (some-> kind str/lower-case)]
+     (if kind-lower
+       ;; Filtered to a single kind
+       (cond
+         (= "temperature" kind-lower)
+         (format-temp-section aliases)
+
+         (= "compound" kind-lower)
+         (format-compound-section)
+
+         (get label->dim kind-lower)
+         (let [dim (get label->dim kind-lower)
+               entries (get by-dim dim)
+               label (get dim-labels dim)]
+           (if entries
+             (format-section label entries aliases)
+             (str "No units found for kind: " kind)))
+
+         :else
+         (str "Unknown kind: " kind "\nAvailable: " (str/join ", " all-kind-names)))
+       ;; Show all
+       (let [sections
+             (for [dim dim-order
+                   :let [entries (get by-dim dim)]
+                   :when entries]
+               (format-section (get dim-labels dim (pr-str dim)) entries aliases))]
+         (str/join "\n\n" (concat sections
+                                  [(format-temp-section aliases)
+                                   (format-compound-section)])))))))
+
 (defn format-number
   ([x] (format-number x nil))
   ([x {:keys [precision sig-figs] :as opts}]
@@ -61,10 +156,13 @@
    "\n"
    ["Usage:"
     "  unitz <request>"
+    "  unitz --list [--kind <kind>]"
     ""
     "Examples:"
     "  unitz 12 feet in yards"
-    "  unitz 2 cubic yards to US liquid gallons"]))
+    "  unitz 2 cubic yards to US liquid gallons"
+    "  unitz --list"
+    "  unitz --list --kind length"]))
 
 (defn parse-format-opts [tokens]
   (loop [remaining tokens
@@ -95,19 +193,45 @@
           :else
           (recur (rest remaining) opts (conj result t)))))))
 
+(defn extract-flag
+  "Remove a flag and its argument from tokens. Returns [value remaining-tokens]."
+  [tokens flag-short flag-long]
+  (loop [remaining tokens
+         value nil
+         result []]
+    (if (empty? remaining)
+      [value result]
+      (let [t (first remaining)]
+        (if (and (or (= flag-short t) (= flag-long t))
+                 (second remaining))
+          (recur (drop 2 remaining) (second remaining) result)
+          (recur (rest remaining) value (conj result t)))))))
+
 (defn -main [& args]
   (try
     (let [input (str/trim (str/join " " args))
           tokens (str/split input #"\s+")
+          list? (some #{"--list"} tokens)
+          tokens (vec (remove #{"--list"} tokens))
+          [kind tokens] (extract-flag tokens "-k" "--kind")
           verbose? (some #{"-v" "--verbose"} tokens)
           tokens (vec (remove #{"-v" "--verbose"} tokens))
           [fmt-opts tokens] (parse-format-opts tokens)
           input (str/trim (str/join " " tokens))]
-      (if (str/blank? input)
+      (cond
+        list?
+        (println (list-units kind))
+
+        (and kind (not list?))
+        (println (list-units kind))
+
+        (str/blank? input)
         (do
           (binding [*out* *err*]
             (println (usage)))
           (System/exit 1))
+
+        :else
         (let [{:keys [error result from target]} (process-request-text input fmt-opts)]
           (if error
             (do
